a2c9e49ac9f52ba8650ecb20d167ada9ea611122
Christian Hermann <c.hermann@bitbeans.de>
2015-10-16 09:06:33 +0200

    Version 0.2.0-beta
    - Added Nuget package
    - Updated examples
    - Updated README

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# knownpasswords.org C# bindings
+# knownpasswords-net [![NuGet Version](https://img.shields.io/nuget/v/knownpasswords-net.svg?style=flat-square)](https://www.nuget.org/packages/knownpasswords-net/) [![License](http://img.shields.io/badge/license-MIT-green.svg?style=flat-square)](https://github.com/bitbeans/knownpasswords-net/blob/master/LICENSE.md)
+
+knownpasswords.org C# bindings
+
+## Requirements
+
+Just a knownpasswords.org API Key: [Get an API key](https://knownpasswords.org/)
+
+## Installation
+
+There is a [NuGet package](https://www.nuget.org/packages/knownpasswords-net/) available.
+
+## Example
+
+```csharp
+	
+	// check 'monkey' as Blake2b hash
+	const string blake2b = "931f2b3f873fd41e0481972a7faa4ec65723867197c52d7287cc0eb0cab8c439e4ba27b427ff5dc18ae268e39a8f488a9639714cdc680964d0bd7f0133e0af24"; 
+	
+	var knownPasswords = new KnownPasswords("<your private API key>");
+	var response = knownPasswords.CheckPassword(blake2b, PasswordFormatType.Blake2b);
+	if (response.FoundPassword)
+	{
+		//password is a public known password
+		//prevent registration or warn the user
+	}
+	else
+	{
+		//password is not known by API
+		//use a KDF, encrypt the password and store it
+	}
+```
+
+## Note
+
+knownpasswords.org can validate the following password formats:
+
+- Blake2b (64 byte hash)
+- Sha512 (64 byte hash)
+- Cleartext password
+
+Never store passwords in these formats, always use a KDF (key derivation function)!
+libsodium supports scrypt.
+
+- A response never contains the cleartext password.
+- The API is static and will not add requested passwords.
+- Requests and responses are always signed and encrypted.
+- The API is only reachable over https
+
+## License
+
+[MIT](https://en.wikipedia.org/wiki/MIT_License)
--- Tests/RequestTests.cs
+++ Tests/RequestTests.cs
@@ -15,27 +15,27 @@ namespace Tests
         [Test]
         public void CheckPasswordClearText()
         {
-            const string password = "password123";
+            const string cleartext = "password123";
             var knownPasswords = new KnownPasswords(_clientPrivateKey);
-            var response = knownPasswords.CheckPassword(password, PasswordFormatType.Cleartext);
+            var response = knownPasswords.CheckPassword(cleartext, PasswordFormatType.Cleartext);
             Assert.AreEqual(true, response.FoundPassword);
         }
 
         [Test]
         public void CheckPasswordBlake2b()
         {
-            const string password = "password123";
+            const string blake2b = "fbdba996cade3bae2d948c2f03f8149ffa7068584731ac6efbef1688e64609b6969a52dcc203b74aa87d6d9d1b0cd93bea724cddd12443f2b808bc03776b81cc";
             var knownPasswords = new KnownPasswords(_clientPrivateKey);
-            var response = knownPasswords.CheckPassword(ApiHelper.ConvertPasswordToBlake2b(password), PasswordFormatType.Blake2b);
+            var response = knownPasswords.CheckPassword(blake2b, PasswordFormatType.Blake2b);
             Assert.AreEqual(true, response.FoundPassword);
         }
 
         [Test]
         public void CheckPasswordSha512()
         {
-            const string password = "password123";
+            const string sha512 = "bed4efa1d4fdbd954bd3705d6a2a78270ec9a52ecfbfb010c61862af5c76af1761ffeb1aef6aca1bf5d02b3781aa854fabd2b69c790de74e17ecfec3cb6ac4bf";
             var knownPasswords = new KnownPasswords(_clientPrivateKey);
-            var response = knownPasswords.CheckPassword(ApiHelper.ConvertPasswordToSha512(password), PasswordFormatType.Sha512);
+            var response = knownPasswords.CheckPassword(sha512, PasswordFormatType.Sha512);
             Assert.AreEqual(true, response.FoundPassword);
         }
 
